08f8a69947494a93b0adbcec78e47a96ecf3f2ee
pisalsek <pisalsek99@gmail.com>
2023-05-06 18:22:30 +0700

    L

---
+++ src/La.java
@@ -0,0 +1,6 @@
+public class La {
+    public static void main(String[] args) {
+        int a =20;
+        System.out.println("a="+a);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,6 @@ public class Main {
         var a = 10;
         System.out.println(a);
         System.out.println("");
+
     }
 }
