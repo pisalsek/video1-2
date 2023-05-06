57703487aa9906fca74592a523d548a390afaa34
pisalsek <pisalsek99@gmail.com>
2023-05-06 21:08:39 +0700

    sal

--- src/La.java
+++ src/La.java
@@ -1,6 +1,15 @@
+import java.text.DecimalFormat;
+
 public class La {
     public static void main(String[] args) {
         int a =20;
+        double salary = 99.456647;
+
         System.out.println("a="+a);
+
+        DecimalFormat df=new DecimalFormat("$0.##");
+        System.out.println("My salary is : "+df.format(salary));
+
+
     }
 }
